d0873a0660557764829860b32f5e38247ad3d535
NidoSen <2820147082@qq.com>
2023-06-03 19:36:37 +0800

    项目骨架搭建完成

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -13,16 +13,10 @@ import java.util.Map;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getBotInfo(){
-        List<Map<String, String>> list = new LinkedList<>();
+    public Map<String, String> getBotInfo(){
         Map<String, String> bot1 = new HashMap<>();
         bot1.put("name", "tiger");
         bot1.put("rating", "1500");
-        Map<String, String> bot2 = new HashMap<>();
-        bot2.put("name", "apple");
-        bot2.put("rating", "1800");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+        return bot1;
     }
 }
